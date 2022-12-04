291dd0b5fda217abfc0d50a45f5f1350cbb21fa5
Anže Oblak <anze.oblak@rettro.si>
2022-12-04 16:53:05 +0100

    Added api endpoints

--- api/src/main/java/si/fri/rso/shoppingcart/api/v1/resources/ShoppingCartResource.java
+++ api/src/main/java/si/fri/rso/shoppingcart/api/v1/resources/ShoppingCartResource.java
@@ -23,7 +23,6 @@ import java.util.List;
 import java.util.logging.Logger;
 
 
-
 @ApplicationScoped
 @Path("/shopping-cart")
 @Produces(MediaType.APPLICATION_JSON)
@@ -33,129 +32,125 @@ public class ShoppingCartResource {
     private Logger log = Logger.getLogger(ShoppingCartResource.class.getName());
 
     @Inject
-    private ShoppingCartBean imageMetadataBean;
+    private ShoppingCartBean shoppingCartBean;
 
 
     @Context
     protected UriInfo uriInfo;
 
-//    @Operation(description = "Get all image metadata.", summary = "Get all metadata")
-//    @APIResponses({
-//            @APIResponse(responseCode = "200",
-//                    description = "List of image metadata",
-//                    content = @Content(schema = @Schema(implementation = ShoppingCart.class, type = SchemaType.ARRAY)),
-//                    headers = {@Header(name = "X-Total-Count", description = "Number of objects in list")}
-//            )})
-//    @GET
-//    public Response getImageMetadata() {
-//
-//        List<ShoppingCart> imageMetadata = imageMetadataBean.getImageMetadataFilter(uriInfo);
-//
-//        return Response.status(Response.Status.OK).entity(imageMetadata).build();
-//    }
-//
-//
-//    @Operation(description = "Get metadata for an image.", summary = "Get metadata for an image")
-//    @APIResponses({
-//            @APIResponse(responseCode = "200",
-//                    description = "Image metadata",
-//                    content = @Content(
-//                            schema = @Schema(implementation = ShoppingCart.class))
-//            )})
-//    @GET
-//    @Path("/{imageMetadataId}")
-//    public Response getImageMetadata(@Parameter(description = "Metadata ID.", required = true)
-//                                     @PathParam("imageMetadataId") Integer imageMetadataId) {
-//
-//        ShoppingCart imageMetadata = imageMetadataBean.getImageMetadata(imageMetadataId);
-//
-//        if (imageMetadata == null) {
-//            return Response.status(Response.Status.NOT_FOUND).build();
-//        }
-//
-//        return Response.status(Response.Status.OK).entity(imageMetadata).build();
-//    }
-//
-//    @Operation(description = "Add image metadata.", summary = "Add metadata")
-//    @APIResponses({
-//            @APIResponse(responseCode = "201",
-//                    description = "Metadata successfully added."
-//            ),
-//            @APIResponse(responseCode = "405", description = "Validation error .")
-//    })
-//    @POST
-//    public Response createImageMetadata(@RequestBody(
-//            description = "DTO object with image metadata.",
-//            required = true, content = @Content(
-//            schema = @Schema(implementation = ShoppingCart.class))) ShoppingCart imageMetadata) {
-//
-//        if ((imageMetadata.getTitle() == null || imageMetadata.getDescription() == null || imageMetadata.getUri() == null)) {
-//            return Response.status(Response.Status.BAD_REQUEST).build();
-//        }
-//        else {
-//            imageMetadata = imageMetadataBean.createImageMetadata(imageMetadata);
-//        }
-//
-//        return Response.status(Response.Status.CONFLICT).entity(imageMetadata).build();
-//
-//    }
-//
-//
-//    @Operation(description = "Update metadata for an image.", summary = "Update metadata")
-//    @APIResponses({
-//            @APIResponse(
-//                    responseCode = "200",
-//                    description = "Metadata successfully updated."
-//            )
-//    })
-//    @PUT
-//    @Path("{imageMetadataId}")
-//    public Response putImageMetadata(@Parameter(description = "Metadata ID.", required = true)
-//                                     @PathParam("imageMetadataId") Integer imageMetadataId,
-//                                     @RequestBody(
-//                                             description = "DTO object with image metadata.",
-//                                             required = true, content = @Content(
-//                                             schema = @Schema(implementation = ShoppingCart.class)))
-//                                             ShoppingCart imageMetadata){
-//
-//        imageMetadata = imageMetadataBean.putImageMetadata(imageMetadataId, imageMetadata);
-//
-//        if (imageMetadata == null) {
-//            return Response.status(Response.Status.NOT_FOUND).build();
-//        }
-//
-//        return Response.status(Response.Status.NOT_MODIFIED).build();
-//
-//    }
-//
-//    @Operation(description = "Delete metadata for an image.", summary = "Delete metadata")
-//    @APIResponses({
-//            @APIResponse(
-//                    responseCode = "200",
-//                    description = "Metadata successfully deleted."
-//            ),
-//            @APIResponse(
-//                    responseCode = "404",
-//                    description = "Not found."
-//            )
-//    })
-//    @DELETE
-//    @Path("{imageMetadataId}")
-//    public Response deleteImageMetadata(@Parameter(description = "Metadata ID.", required = true)
-//                                        @PathParam("imageMetadataId") Integer imageMetadataId){
-//
-//        boolean deleted = imageMetadataBean.deleteImageMetadata(imageMetadataId);
-//
-//        if (deleted) {
-//            return Response.status(Response.Status.NO_CONTENT).build();
-//        }
-//        else {
-//            return Response.status(Response.Status.NOT_FOUND).build();
-//        }
-//    }
-//
-//
-//
+    @Operation(description = "Get all shopping carts.", summary = "Get all shopping carts")
+    @APIResponses({
+            @APIResponse(responseCode = "200",
+                    description = "List of shopping carts.",
+                    content = @Content(schema = @Schema(implementation = ShoppingCart.class, type = SchemaType.ARRAY)),
+                    headers = {@Header(name = "X-Total-Count", description = "Number of objects in list")}
+            )})
+    @GET
+    public Response getShoppingCarts() {
+
+        List<ShoppingCart> shoppingCarts = shoppingCartBean.getShoppingCarts();
+
+        return Response.status(Response.Status.OK).entity(shoppingCarts).build();
+    }
 
 
+    @Operation(description = "Get shopping cart by id.", summary = "Get shopping cart by id")
+    @APIResponses({
+            @APIResponse(responseCode = "200",
+                    description = "Shopping cart information.",
+                    content = @Content(
+                            schema = @Schema(implementation = ShoppingCart.class))
+            ),
+            @APIResponse(
+                    responseCode = "404",
+                    description = "Shopping cart not found."
+            )})
+    @GET
+    @Path("/{shoppingCartId}")
+    public Response getShoppingCartById(@Parameter(description = "Shopping cart ID.", required = true)
+                                     @PathParam("shoppingCartId") Integer shoppingCartId) {
+
+        ShoppingCart shoppingCart = shoppingCartBean.getShoppingCartById(shoppingCartId);
+
+        if (shoppingCart == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        return Response.status(Response.Status.OK).entity(shoppingCart).build();
+    }
+
+    @Operation(description = "Add shopping cart.", summary = "Add shopping cart")
+    @APIResponses({
+            @APIResponse(responseCode = "201",
+                    description = "Shopping cart successfully added."
+            ),
+            @APIResponse(responseCode = "405", description = "Validation error.")
+    })
+    @POST
+    public Response createShoppingCart(@RequestBody(
+            description = "DTO object with shopping cart data.",
+            required = true, content = @Content(
+            schema = @Schema(implementation = ShoppingCart.class))) ShoppingCart shoppingCart) {
+
+        if (shoppingCart.getProducts() == null) {
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        } else {
+            shoppingCart = shoppingCartBean.createShoppingCart(shoppingCart);
+        }
+
+        return Response.status(Response.Status.CONFLICT).entity(shoppingCart).build();
+    }
+
+
+    @Operation(description = "Update shopping cart data.", summary = "Update shoping cart")
+    @APIResponses({
+            @APIResponse(
+                    responseCode = "200",
+                    description = "Shopping cart successfully updated."
+            )
+    })
+    @PUT
+    @Path("{shoppingCartId}")
+    public Response putShoppingCart(@Parameter(description = "Shopping cart ID.", required = true)
+                                     @PathParam("shoppingCartId") Integer shoppingCartId,
+                                     @RequestBody(
+                                             description = "DTO object with shopping cart data.",
+                                             required = true, content = @Content(
+                                             schema = @Schema(implementation = ShoppingCart.class)))
+                                             ShoppingCart shoppingCart) {
+
+        shoppingCart = shoppingCartBean.putShoppingCart(shoppingCartId, shoppingCart);
+
+        if (shoppingCart == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        return Response.status(Response.Status.NOT_MODIFIED).build();
+    }
+
+
+    @Operation(description = "Delete shopping cart.", summary = "Delete shopping cart")
+    @APIResponses({
+            @APIResponse(
+                    responseCode = "200",
+                    description = "Shopping cart successfully deleted."
+            ),
+            @APIResponse(
+                    responseCode = "404",
+                    description = "Shopping cart not found."
+            )
+    })
+    @DELETE
+    @Path("{shoppingCartId}")
+    public Response deleteShoppingCart(@Parameter(description = "Shopping cart ID.", required = true)
+                                        @PathParam("shoppingCartId") Integer shoppingCartId) {
+
+        boolean deleted = shoppingCartBean.deleteShoppingCart(shoppingCartId);
+
+        if (deleted) {
+            return Response.status(Response.Status.NO_CONTENT).build();
+        } else {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+    }
 }
